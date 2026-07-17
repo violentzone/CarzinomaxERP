80f82c74b1a9841720333aca6eebe417573ba783
violentzone <farewell.torture@gmail.com>
2026-07-18 01:23:21 +0800

    Employee tool varible establish done

--- backend/ai_service/tools/hr.py
+++ backend/ai_service/tools/hr.py
@@ -3,18 +3,22 @@ from typing import Literal
 from datetime import datetime
 
 
+
 from app.models import Employee
 from uuid import uuid4
 
-def operate_employee(operation: Literal['create', 'update', 'delete'], name: str, department_id: int=None, email: str|None=None, job_title: str|None=None) -> bool:
+def operate_employee(operation: Literal['create', 'update', 'delete'], name: str, salary: float, department_id: int=None, email: str|None=None, job_title: str|None=None,
+                     ) -> bool:
     """
     Tool for LLM to operate on Employee table in database
     Args:
         operation (str): CRUD operation in string format
         name (str): Employee name
+        salary (float): Employee salary
         department_id (int)(optional): Department id of employee, required if create employee or update to different department
         email (str): Employee email
         job_title (str): Employee job title
+
 
     Returns:
         True if successful, False otherwise
@@ -22,4 +26,4 @@ def operate_employee(operation: Literal['create', 'update', 'delete'], name: str
     if operation == 'create':
         _id = uuid4()
         hire_date = datetime.now().date()
-        employee = Employee(name=name, department_id=department_id, id=_id, email=email, hire_date=hire_date)
+        employee = Employee(name=name, salary=salary, department_id=department_id, id=_id, email=email, hire_date=hire_date, job_title=job_title, status='active', )
