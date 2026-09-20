758c73d77788ac98f0df5c9f3d90726922c604ae
violentzone <farewell.torture@gmail.com>
2026-09-20 18:39:44 +0800

    Build login from scratch

--- backend/app/api/v1/auth.py
+++ backend/app/api/v1/auth.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from app.core.database import get_db
 
 auth_router = APIRouter()
 
+@auth_router.post("/login")
+async def login(user_email: str, password: str, db=Depends(get_db)):
+    """
+    Login endpoint
+    Args:
+        user_email: User input Email
+        password: User input Password
+        db: Database connection, defined in app.core.database
+
+    Returns:
+
+    """
